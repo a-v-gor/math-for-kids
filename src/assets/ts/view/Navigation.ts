a2394cb44320619a91da28d6f5405bee5e0622a9
Alexey Gorbenko <a.v.gor@mail.ru>
2024-08-30 22:01:51 +0300

    refactor: start add Navigator class

---
+++ src/assets/ts/view/Navigation.ts
@@ -0,0 +1,24 @@
+// import returnElement from "./returnElement";
+
+// export default class Navigation {
+//   nav: HTMLElement;
+
+//   constructor() {
+//     this.nav = returnElement({
+//       tag: 'nav',
+//       classes: ['menu'],
+//     });
+//     const navUl = returnElement({
+//       tag: 'ul',
+//       classes: ['menu__ul'],
+//     });
+//     this.navHome = <HTMLLIElement>returnElement({
+//       tag: 'li',
+//       classes: ['menu__item', 'menu__home'],
+//     });
+
+//     navUl.append(this.navHome);
+//     nav.append(navUl);
+//   }
+
+// }
